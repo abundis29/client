d156332997137598d4b96922e7f7e817dca3f815
Ivan A <heysoyivan@gmail.com>
2022-03-09 01:50:48 -0600

    Fix Husky 2022:1:12

--- scripts/pre-commit.js
+++ scripts/pre-commit.js
@@ -1,6 +1,6 @@
 var spawnSync = require('child_process').spawnSync
 const result = spawnSync('npm run validate --silent', { stdio: 'inherit', shell: true })
 if (result.status !== 0) {
-  console.log('ALL GOOD ✅')
   process.exit(result.status)
 }
+console.log('ALL GOOD ✅')
